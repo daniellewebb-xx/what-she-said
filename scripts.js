4517a569abe9a9fdee429b4b73da8df823a9bf41
Danielle Webb <danielle.a.webb@gmail.com>
2015-03-08 11:27:40 -0400

    added social media buttons

--- scripts.js
+++ scripts.js
@@ -65,7 +65,7 @@ encouragement = [
 	},
 	// Need a little boost
 	{
-		"quote" : "But the world would be a better place if more engineers, like me, hated technology. The stuff I design, if I’m successful, nobody will ever notice.",
+		"quote" : "The world would be a better place if more engineers, like me, hated technology. The stuff I design, if I’m successful, nobody will ever notice.",
 		"attribute" : "Radia Perlman",
 		"wikipedia" : "http://en.wikipedia.org/wiki/Radia_Perlman"
 	},
@@ -107,7 +107,24 @@ $("select").change(function(){
 	$('.words-to-live-by a').attr('href',link);
 });
 
+// Social Media buttons
+!function(d,s,id) {
+	var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
+	if(!d.getElementById(id)){
+		js=d.createElement(s);
+		js.id=id;
+		js.src=p+'://platform.twitter.com/widgets.js';
+		fjs.parentNode.insertBefore(js,fjs);
+	}
+}(document, 'script', 'twitter-wjs');
 
+(function(d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) return;
+	js = d.createElement(s); js.id = id;
+	js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
+	fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
 
 
 
